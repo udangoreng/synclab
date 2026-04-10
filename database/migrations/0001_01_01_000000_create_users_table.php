5fe83d9e685011fa8142adda1c920443a85e6a01
udangoreng <divanandasasikirana@gmail.com>
2026-04-10 20:51:59 +0700

    Set Initial migration, controller, and model. Changes env db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('nomor_induk');
             $table->string('nama');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['Praktikan', 'Asisten', 'Dosen', 'Admin',]);
             $table->rememberToken();
             $table->timestamps();
         });
