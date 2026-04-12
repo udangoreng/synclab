217ee01d8fbb283686d04621d55f01aab5016bd8
udangoreng <divanandasasikirana@gmail.com>
2026-04-12 12:13:24 +0700

    Turn HTML into blade

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('mahasiswa/dashboard');
+});
+Route::get('/c', function () {
+    return view('mahasiswa/praktikum');
+});
+Route::get('/d', function () {
+    return view('mahasiswa/pendaftaran');
+});
+Route::get('/a', function () {
+    return view('mahasiswa/presensi');
+});
+Route::get('/t', function () {
+    return view('mahasiswa/pretest');
+});
+Route::get('/r', function () {
+    return view('mahasiswa/riwayat');
+});
+Route::get('/n', function () {
+    return view('mahasiswa/nilai');
 });
